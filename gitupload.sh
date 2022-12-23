82e6793167b765c6827a4ceb11f8c77a17ad43eb
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-23 11:44:34 +0100

    modification

--- gitupload.sh
+++ gitupload.sh
@@ -3,4 +3,5 @@ git add .
 git remote add origin https://github.com/vishnuiot/project6.git
 git commit -m "modification"
 git branch -M main
-git pull origin master
+git checkout main
+git pull origin main
